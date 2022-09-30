554aec0cf6875c52239a73c1da9f00461573970d
Marcos Rodrigues <mquinhoz@hotmail.com>
2022-09-30 14:54:23 -0300

    Control finance: first release - static elements and styles

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,7 @@
-/* Desenvolva sua lógica aqui */
+const showModal = document.createElement("div");
+showModal.classList.add("show-modal")
+
+function modalAsign(input){
+    
+    const modal = document.createElement("div")
+}
